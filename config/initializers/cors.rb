b0166fa488df63600532189dff23d33fff18c689
waykwo <72991933+waykwo@users.noreply.github.com>
2024-10-22 20:49:00 -0400

    Reconfig cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'mini-capstone.peterxjang.com', "localhost:5173"
+    origins "localhost:5173", "mortgage-calc-frontend.onrender.com"
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
